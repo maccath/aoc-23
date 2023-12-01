5d1b45b1b0e260a25360f9ac70ef60f455a44c15
Katy Ereira <katy@skillshare.com>
2023-12-01 19:26:47 +0000

    Initial commit.

---
+++ tests/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maccath\Aoc23\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
